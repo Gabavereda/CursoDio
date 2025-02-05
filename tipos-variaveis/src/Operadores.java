e1fc1b36c0464e844e68949cb3e4127627ca5490
Glauber Vereda <gabavereda@gmail.com>
2025-02-05 15:43:40 -0300

    operardor ternario

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,6 +2,18 @@ public class Operadores {
 
     public static void main(String[] args) {
 
+        String nomeCompleto = "nome1" + "nome2";
+        System.out.println(nomeCompleto);
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        // ternario
+        String resultado = a == b ? "verdade" : "falso";
+
+        System.out.println(resultado);
+
     }
 
 }
