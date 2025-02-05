9378fb5d2ccb62436b3478279e9dc1fb08def3b6
Glauber Vereda <gabavereda@gmail.com>
2025-02-05 09:03:45 -0300

    tipos de  variaveis

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // var xx const
+
+        // usando final sempre a variavel tem que ser maiuscula
+
+        final double VALOR_PI = 3.14;
+        
     }
 }
