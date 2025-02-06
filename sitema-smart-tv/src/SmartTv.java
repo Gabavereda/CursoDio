0247f9279c9e74b2ecd522cfd3bf171de945e74b
Glauber Vereda <gabavereda@gmail.com>
2025-02-06 09:37:00 -0300

    feat: novos metodos

--- sitema-smart-tv/src/SmartTv.java
+++ sitema-smart-tv/src/SmartTv.java
@@ -4,6 +4,16 @@ public class SmartTv {
     int canal = 1;
     int volume = 25;
 
+    public void aumentarVolume() {
+
+        volume++;
+    }
+
+    public void diminuirVolume() {
+
+        volume--;
+    }
+
     public void ligar() {
         ligada = true;
 
@@ -12,6 +22,19 @@ public class SmartTv {
     public void desligar() {
 
         ligada = false;
+    }
+
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal() {
+        canal++;
+    }
+
+    public void diminuirCanal() {
+        canal--;
+
     }
 
 }
