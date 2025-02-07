9c18ba23bfcb9333abc209fc2b125113e0c8f859
Glauber Vereda <gabavereda@gmail.com>
2025-02-07 08:44:37 -0300

    metodo

--- terminalTips/src/MinhaClasse.java
+++ terminalTips/src/MinhaClasse.java
@@ -1,5 +1,13 @@
 public class MinhaClasse {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Terminal!");
+
+        public class Tv {
+            int canal = 1;
+
+            void mudarCanal(int novoCanal) {
+                canal = novoCanal;
+            }
+        }
     }
 }
