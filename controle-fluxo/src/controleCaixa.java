723c6a37c81a87308754de0f974e1bdb685c6f1b
Glauber Vereda <gabavereda@gmail.com>
2025-02-07 08:50:44 -0300

    feat: fluxo

---
+++ controle-fluxo/src/controleCaixa.java
@@ -0,0 +1,15 @@
+public class controleCaixa {
+    public static void main(String[] args) throws Exception {
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if (valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+
+            System.out.println(saldo);
+        } else {
+
+        }
+
+    }
+}
