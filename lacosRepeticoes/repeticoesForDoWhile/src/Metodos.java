7c7a875f269e6c59095b653457da14e85c4c6112
Glauber Vereda <gabavereda@gmail.com>
2025-02-07 09:57:41 -0300

    update: made some adjusts

--- lacosRepeticoes/repeticoesForDoWhile/src/Metodos.java
+++ lacosRepeticoes/repeticoesForDoWhile/src/Metodos.java
@@ -1,4 +1,5 @@
 public class Metodos {
+    String alunos[] = { "Felipe", "Claudio", "Gomes" };
 
     public void forParaCarneiros() {
 
@@ -9,4 +10,19 @@ public class Metodos {
 
     }
 
+    public void arrayExercicio() {
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("o aluno " + i + "é de nome" + alunos[i]);
+        }
+    }
+
+    public void forEachExercicio() {
+
+        for (String aluno : alunos) {
+            System.out.println("nome do aluno é " + aluno);
+
+        }
+
+    }
 }
