c06d09c418a3905424f15927b4022f43aac01b8b
Glauber Vereda <gabavereda@gmail.com>
2025-02-07 09:48:06 -0300

    feat: create count

---
+++ lacosRepeticoes/repeticoesForDoWhile/src/Metodos.java
@@ -0,0 +1,12 @@
+public class Metodos {
+
+    public void forParaCarneiros() {
+
+        for (int carneiros = 0; carneiros <= 20; carneiros++) {
+            System.out.println("count sheeps " + carneiros);
+
+        }
+
+    }
+
+}
