1b107b0cb183687ab8a272bb97d9269f32bb19f4
Glauber Vereda <gabavereda@gmail.com>
2025-02-07 09:01:56 -0300

    feat: responsibility

--- controle-fluxo/src/Metodos.java
+++ controle-fluxo/src/Metodos.java
@@ -11,4 +11,18 @@ public class Metodos {
         }
 
     }
+
+    public void caixa() {
+    
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if (valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+
+            System.out.println(saldo);
+        } else {
+
+        }
+    }
 }
--- controle-fluxo/src/controleCaixa.java
+++ controle-fluxo/src/controleCaixa.java
@@ -1,19 +1,10 @@
 public class ControleCaixa {
 
     public static void main(String[] args) throws Exception {
-        double saldo = 25.0;
-        double valorSolicitado = 17.0;
-
-        if (valorSolicitado < saldo) {
-            saldo = saldo - valorSolicitado;
-
-            System.out.println(saldo);
-        } else {
-
-        }
 
         Metodos metodos = new Metodos();
 
+        metodos.caixa();
         metodos.boletim();
 
     }
