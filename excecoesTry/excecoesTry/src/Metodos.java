c44e84af99589500037163405993fe326919776e
Glauber Vereda <gabavereda@gmail.com>
2025-02-10 09:42:06 -0300

    feat: applying try/catch

--- excecoesTry/excecoesTry/src/Metodos.java
+++ excecoesTry/excecoesTry/src/Metodos.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Metodos {
@@ -10,21 +11,26 @@ public class Metodos {
 
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Qual o seu nome");
+        try {
+            System.out.println("Qual o seu nome");
 
-        nome = entrada.nextLine();
+            nome = entrada.nextLine();
 
-        System.out.println("Seu cpf");
+            System.out.println("Seu cpf");
 
-        cpf = entrada.nextLine();
+            cpf = entrada.nextLine();
 
-        System.out.println("Seu telefone ");
+            System.out.println("Seu telefone ");
 
-        telefone = entrada.nextLine();
+            telefone = entrada.nextLine();
 
-        System.out.println("e por fim sua altura ");
+            System.out.println("e por fim sua altura ");
 
-        altura = entrada.nextDouble();
+            altura = entrada.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("Os dados foram errados");
+
+        }
 
     }
 
