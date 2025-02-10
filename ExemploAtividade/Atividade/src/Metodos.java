f60b9c5017a7434faeecf855e65c64fcee0312c1
Glauber Vereda <gabavereda@gmail.com>
2025-02-10 11:22:29 -0300

    feat: selecao

--- ExemploAtividade/Atividade/src/Metodos.java
+++ ExemploAtividade/Atividade/src/Metodos.java
@@ -1,3 +1,45 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Metodos {
+    double salarioBase = 2000.0;
+
+    public void analisarCandidato(double salarioPretendido) {
+
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar candidato");
+
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("ligar com contraProposta");
+
+        } else {
+            System.out.println("aguardar outros");
+        }
+
+    }
+
+    public void selecaoCandidatos() {
+        String candidatos[] = { "carlos", "felipe", "gomes" };
+        int candidatoSelecionados = 0;
+        int candidatoAtual = 0;
+
+        while (candidatoSelecionados < 2) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("cadidato " + candidato + " Solicitou eset valor" + salarioPretendido);
+            if (salarioBase >= salarioPretendido)
+                System.out.println("candidato " + candidato + " selecionado");
+            candidatoSelecionados++;
+            {
+                candidatoAtual++;
+
+            }
+
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
 
 }
