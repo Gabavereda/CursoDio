c8f49757d4d2861732dc048e8dd840d183a89c01
Glauber Vereda <gabavereda@gmail.com>
2025-03-18 22:11:41 -0300

    feat:: add custom queries to UserRepository for filtering TestesDio entities

--- IntroducaoSpring/aula-spring-data-jpa/src/main/java/com/gaba/aula_spring_data_jpa/repository/UserRepository.java
+++ IntroducaoSpring/aula-spring-data-jpa/src/main/java/com/gaba/aula_spring_data_jpa/repository/UserRepository.java
@@ -1,9 +1,23 @@
 package com.gaba.aula_spring_data_jpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.gaba.aula_spring_data_jpa.model.TestesDio;
 
 public interface UserRepository extends JpaRepository<TestesDio, Integer> {
+
+    // Query
+    List<TestesDio> findByNameContainig(String name);
+
+    // Query
+    TestesDio findByUserName(String username);
+
+    // Query Override
+    @Query("SELECT u testesDio u WHERE u.name LIKE %:name%")
+    List<TestesDio> filtrarPorNome(@Param("name") String name);
 
 }
