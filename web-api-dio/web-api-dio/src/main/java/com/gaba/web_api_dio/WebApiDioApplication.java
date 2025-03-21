15283653a5cd7d367db2a7d80710bb655e148cb0
Glauber Vereda <gabavereda@gmail.com>
2025-03-21 11:17:19 -0300

    feat:: initialize web-api-dio project with basic structure and configuration files

---
+++ web-api-dio/web-api-dio/src/main/java/com/gaba/web_api_dio/WebApiDioApplication.java
@@ -0,0 +1,13 @@
+package com.gaba.web_api_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiDioApplication.class, args);
+	}
+
+}
---
+++ web-api-dio/web-api-dio/src/test/java/com/gaba/web_api_dio/WebApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaba.web_api_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
