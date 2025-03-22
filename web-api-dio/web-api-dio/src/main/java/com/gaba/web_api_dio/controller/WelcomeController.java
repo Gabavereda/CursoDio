10f99e7240b3a21d906affa5dbd4eb2cd9de14d6
Glauber Vereda <gabavereda@gmail.com>
2025-03-22 09:29:23 -0300

    feat:: update WelcomeController to remove path and add Usuario model

--- web-api-dio/web-api-dio/src/main/java/com/gaba/web_api_dio/controller/WelcomeController.java
+++ web-api-dio/web-api-dio/src/main/java/com/gaba/web_api_dio/controller/WelcomeController.java
@@ -3,7 +3,7 @@ package com.gaba.web_api_dio.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/welcome")
+@RestController
 public class WelcomeController {
     @GetMapping
     public String welcome() {
