02013ac703568ef9f8bbee90bd44a40d56543c27
Glauber Vereda <gabavereda@gmail.com>
2025-03-22 10:30:42 -0300

    feat:: add delete and register endpoints to UsuarioController

--- web-api-dio/web-api-dio/src/main/java/com/gaba/web_api_dio/controller/UsuarioController.java
+++ web-api-dio/web-api-dio/src/main/java/com/gaba/web_api_dio/controller/UsuarioController.java
@@ -3,8 +3,11 @@ package com.gaba.web_api_dio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +29,16 @@ public class UsuarioController {
     @GetMapping("buscarNome/{username}")
     public Usuario getOne(@PathVariable("username") String username) {
         return repository.findByUsername(username);
+    }
+
+    @DeleteMapping("deletarId/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        repository.deleteById(id);
+    }
+
+    @PostMapping("cadastrarUsuario")
+    public void cadastrarUsuario(@RequestBody Usuario usuario) {
+        repository.save(usuario);
     }
 
 }
