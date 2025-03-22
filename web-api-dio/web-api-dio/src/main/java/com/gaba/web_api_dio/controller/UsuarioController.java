ff035eb4d31c55a54dcb091564ac1723c870b593
Glauber Vereda <gabavereda@gmail.com>
2025-03-22 10:12:01 -0300

    feat:: enhance UsuarioController with user listing and search by username endpoints

--- web-api-dio/web-api-dio/src/main/java/com/gaba/web_api_dio/controller/UsuarioController.java
+++ web-api-dio/web-api-dio/src/main/java/com/gaba/web_api_dio/controller/UsuarioController.java
@@ -4,20 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gaba.web_api_dio.model.Usuario;
 import com.gaba.web_api_dio.repository.UsuarioRepository;
 
 @RestController
+@RequestMapping("usuarios")
 public class UsuarioController {
 
     @Autowired
     UsuarioRepository repository;
 
-    @GetMapping
+    @GetMapping("listar")
     public List<Usuario> getUsers() {
         return repository.findAll();
+    }
+
+    @GetMapping("buscarNome/{username}")
+    public Usuario getOne(@PathVariable("username") String username) {
+        return repository.findByUsername(username);
     }
 
 }
