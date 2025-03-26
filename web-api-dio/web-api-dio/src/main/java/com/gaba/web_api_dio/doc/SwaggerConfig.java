a0598533126d8f33e6a7be7a310e5ab259c86d72
Glauber Vereda <gabavereda@gmail.com>
2025-03-26 07:07:24 -0300

    feat:: update Swagger configuration to use Springdoc OpenAPI

--- web-api-dio/web-api-dio/src/main/java/com/gaba/web_api_dio/doc/SwaggerConfig.java
+++ web-api-dio/web-api-dio/src/main/java/com/gaba/web_api_dio/doc/SwaggerConfig.java
@@ -1,54 +1,11 @@
 package com.gaba.web_api_dio.doc;
 
-import java.util.Arrays;
-import java.util.HashSet;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig {
-
-    private Contact contato() {
-        return new Contact("Seu nome", "seu site", "voce@seusita");
-    }
-
-    private ApiInfoBuilder informacoesApi() {
-
-        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-
-        apiInfoBuilder.title("Title - Rest Api");
-        apiInfoBuilder.description("Ddescricao ");
-        apiInfoBuilder.version("1.0");
-        apiInfoBuilder.termsOfServiceUrl("Termo do uso OPS");
-        apiInfoBuilder.license("Licenca - sua empresa");
-        apiInfoBuilder.contact(this.contato());
-        return apiInfoBuilder;
-    }
-
-    @Bean
-    public Docket detalheApi() {
-        Docket docket = new Docket(DocumentationType.SWAGGER_2);
-
-        docket
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.gaba.web_api_dio.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(this.informacoesApi().build())
-                .consumes(new HashSet<String>(Arrays.asList("application/json")))
-                .produces(new HashSet<String>(Arrays.asList("application/json")));
-
-        return docket;
-    }
+@OpenAPIDefinition
+public class SwaggerConfig<Docket> {
 
 }
